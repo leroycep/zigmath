de2e4497931e58ccc812cb3d9bf3fafab3f66c84
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-11-29 17:35:54 -0700

    Add Vec type from gamekit

---
+++ math.zig
@@ -0,0 +1,30 @@
+// Define common vector types
+pub const Vec = @import("./vec.zig").Vec;
+pub const Vec2i = Vec(2, i32);
+pub const Vec2u = Vec(2, u32);
+pub const Vec2f = Vec(2, f32);
+pub const Vec3f = Vec(3, f32);
+
+pub fn vec2i(x: i32, y: i32) Vec(2, i32) {
+    return Vec(2, i32).init(x, y);
+}
+
+pub fn vec2u(x: u32, y: u32) Vec(2, u32) {
+    return Vec(2, u32).init(x, y);
+}
+
+pub fn vec2f(x: f32, y: f32) Vec(2, f32) {
+    return Vec(2, f32).init(x, y);
+}
+
+pub fn vec3f(x: f32, y: f32, z: f32) Vec(3, f32) {
+    return Vec(3, f32).init(x, y, z);
+}
+
+pub fn vec2us(x: usize, y: usize) Vec(2, usize) {
+    return Vec(2, usize).init(x, y);
+}
+
+pub fn vec2is(x: isize, y: isize) Vec(2, isize) {
+    return Vec(2, isize).init(x, y);
+}
